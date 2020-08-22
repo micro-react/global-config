7631fbbcf380ec96f75a80b038e9860b2d6fdccb
iWuzhi <2461358597@qq.com>
2020-08-22 13:22:32 +0800

    init: Dev basic support

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * author iWuzhi
+ * date 2020-08-22 13:03:27
+ */
+
+export { default as webpackExternals } from './webpack-externals/index';
---
+++ src/webpack-externals/index.ts
@@ -0,0 +1,9 @@
+/**
+ * author iWuzhi
+ * date 2020-08-22 13:03:59
+ */
+
+export default {
+  react: 'react',
+  reactDOM: 'react-dom',
+}
